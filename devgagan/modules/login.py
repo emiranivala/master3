03f6074bf67268c431785262120d598c0e8ed624
Judhistir <70930235+judhistir88@users.noreply.github.com>
2025-01-11 21:10:22 +0530

    Update login.py

--- devgagan/modules/login.py
+++ devgagan/modules/login.py
@@ -74,41 +74,41 @@ async def generate_session(_, message):
         
         await client.connect()
     except Exception as e:
-        await message.reply(f"❌ Failed to send OTP {e}. Please wait and try again later.")
+        await message.reply(f"❌ Failed to send ➡️ OTP 🗨️ {e}. Please wait 🫷 and try again later.")
     try:
         code = await client.send_code(phone_number)
     except ApiIdInvalid:
-        await message.reply('❌ Invalid combination of API ID and API HASH. Please restart the session.')
+        await message.reply('❌ Invalid combination of API ID and API HASH. Please restart 🔁 the session.')
         return
     except PhoneNumberInvalid:
-        await message.reply('❌ Invalid phone number. Please restart the session.')
+        await message.reply('❌ Invalid phone 📞 number. Please restart 🔁 the session.')
         return
     try:
-        otp_code = await _.ask(user_id, "Please check for an OTP in your official Telegram account. Once received, enter the OTP in the following format: \nIf the OTP is `12345`, please enter it as `1 2 3 4 5`.", filters=filters.text, timeout=600)
+        otp_code = await _.ask(user_id, "Please check for an OTP 🗨️ in your official Telegram account. Once received, enter the OTP in the following format: \nIf the OTP is `12345`, please enter it as `1 2 3 4 5`.", filters=filters.text, timeout=600)
     except TimeoutError:
-        await message.reply('⏰ Time limit of 10 minutes exceeded. Please restart the session.')
+        await message.reply('⏰ Time limit of 10 minutes exceeded. Please restart 🔁 the session.')
         return
     phone_code = otp_code.text.replace(" ", "")
     try:
         await client.sign_in(phone_number, code.phone_code_hash, phone_code)
                 
     except PhoneCodeInvalid:
-        await message.reply('❌ Invalid OTP. Please restart the session.')
+        await message.reply('❌ Invalid OTP. Please restart 🔁 the session.')
         return
     except PhoneCodeExpired:
-        await message.reply('❌ Expired OTP. Please restart the session.')
+        await message.reply('💀 Expired OTP. Please restart 🔁 the session.')
         return
     except SessionPasswordNeeded:
         try:
             two_step_msg = await _.ask(user_id, 'Your account has two-step verification enabled. Please enter your password.', filters=filters.text, timeout=300)
         except TimeoutError:
-            await message.reply('⏰ Time limit of 5 minutes exceeded. Please restart the session.')
+            await message.reply('⏰ Time limit of 5 minutes exceeded. Please restart 🔁 the session.')
             return
         try:
             password = two_step_msg.text
             await client.check_password(password=password)
         except PasswordHashInvalid:
-            await two_step_msg.reply('❌ Invalid password. Please restart the session.')
+            await two_step_msg.reply('❌ Invalid password. Please restart 🔁 the session.')
             return
     string_session = await client.export_session_string()
     await db.set_session(user_id, string_session)

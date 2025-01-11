f6e9a8252e152c5d2c4208b4ee46f653515f83a9
Judhistir <70930235+judhistir88@users.noreply.github.com>
2025-01-11 21:20:00 +0530

    Update gcast.py

--- devgagan/modules/gcast.py
+++ devgagan/modules/gcast.py
@@ -1,5 +1,3 @@
-#devggn
-
 import asyncio
 from pyrogram import filters
 from config import OWNER_ID
@@ -25,9 +23,9 @@ async def send_msg(user_id, message):
 @app.on_message(filters.command("gcast") & filters.user(OWNER_ID))
 async def broadcast(_, message):
     if not message.reply_to_message:
-        await message.reply_text("ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴍᴇssᴀɢᴇ ᴛᴏ ʙʀᴏᴀᴅᴄᴀsᴛ ɪᴛ.")
+        await message.reply_text("Reply to a message to broadcast it.")
         return    
-    exmsg = await message.reply_text("sᴛᴀʀᴛᴇᴅ ʙʀᴏᴀᴅᴄᴀsᴛɪɴɢ!")
+    exmsg = await message.reply_text("Started broadcasting!")
     all_users = (await get_users()) or {}
     done_users = 0
     failed_users = 0
@@ -42,13 +40,12 @@ async def broadcast(_, message):
             failed_users += 1
     if failed_users == 0:
         await exmsg.edit_text(
-            f"**sᴜᴄᴄᴇssғᴜʟʟʏ ʙʀᴏᴀᴅᴄᴀsᴛɪɴɢ ✅**\n\n**sᴇɴᴛ ᴍᴇssᴀɢᴇ ᴛᴏ** `{done_users}` **ᴜsᴇʀs**",
+            f"**Successfully broadcasted ✅**\n\n**Sent message to** `{done_users}` **users**",
         )
     else:
         await exmsg.edit_text(
-            f"**sᴜᴄᴄᴇssғᴜʟʟʏ ʙʀᴏᴀᴅᴄᴀsᴛɪɴɢ ✅**\n\n**sᴇɴᴛ ᴍᴇssᴀɢᴇ ᴛᴏ** `{done_users}` **ᴜsᴇʀs**\n\n**ɴᴏᴛᴇ:-** `ᴅᴜᴇ ᴛᴏ sᴏᴍᴇ ɪssᴜᴇ ᴄᴀɴ'ᴛ ᴀʙʟᴇ ᴛᴏ ʙʀᴏᴀᴅᴄᴀsᴛ` `{failed_users}` **ᴜsᴇʀs**",
+            f"**Successfully broadcasted ✅**\n\n**Sent message to** `{done_users}` **users**\n\n**Note:** `Due to some issues, couldn't broadcast to` `{failed_users}` **users**",
         )
-
 
 
 
@@ -56,29 +53,25 @@ async def broadcast(_, message):
 @app.on_message(filters.command("announce") & filters.user(OWNER_ID))
 async def announced(_, message):
     if message.reply_to_message:
-      to_send=message.reply_to_message.id
+        to_send = message.reply_to_message.id
     if not message.reply_to_message:
-      return await message.reply_text("Reply To Some Post To Broadcast")
+        return await message.reply_text("Reply to a post to broadcast")
     users = await get_users() or []
     print(users)
-    failed_user = 0
+    failed_users = 0
   
     for user in users:
-      try:
-        await _.forward_messages(chat_id=int(user), from_chat_id=message.chat.id, message_ids=to_send)
-        await asyncio.sleep(1)
-      except Exception as e:
-        failed_user += 1
+        try:
+            await _.forward_messages(chat_id=int(user), from_chat_id=message.chat.id, message_ids=to_send)
+            await asyncio.sleep(1)
+        except Exception as e:
+            failed_users += 1
           
     if failed_users == 0:
-        await exmsg.edit_text(
-            f"**sᴜᴄᴄᴇssғᴜʟʟʏ ʙʀᴏᴀᴅᴄᴀsᴛɪɴɢ ✅**\n\n**sᴇɴᴛ ᴍᴇssᴀɢᴇ ᴛᴏ** `{done_users}` **ᴜsᴇʀs**",
+        await mexmsg.edit_text(
+            f"**Successfully broadcasted ✅**\n\n**Sent message to** `{done_users}` **users**",
         )
     else:
         await exmsg.edit_text(
-            f"**sᴜᴄᴄᴇssғᴜʟʟʏ ʙʀᴏᴀᴅᴄᴀsᴛɪɴɢ ✅**\n\n**sᴇɴᴛ ᴍᴇssᴀɢᴇ ᴛᴏ** `{done_users}` **ᴜsᴇʀs**\n\n**ɴᴏᴛᴇ:-** `ᴅᴜᴇ ᴛᴏ sᴏᴍᴇ ɪssᴜᴇ ᴄᴀɴ'ᴛ ᴀʙʟᴇ ᴛᴏ ʙʀᴏᴀᴅᴄᴀsᴛ` `{failed_users}` **ᴜsᴇʀs**",
+            f"**Successfully broadcasted ✅**\n\n**Sent message to** `{done_users}` **users**\n\n**Note:** `Due to some issues, couldn't broadcast to` `{failed_users}` **users**",
         )
-
-
-
-
